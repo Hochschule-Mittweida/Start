329b05754ff2643011b6fa974ce716aff26e2f2b
Tom Schreiber <schreib3@hs-mittweida.de>
2021-08-12 10:59:36 +0200

    Direkte Zuweisung

--- invite/invite.py
+++ invite/invite.py
@@ -5,7 +5,7 @@ import requests
 # Lets go
 print("HSMW: Invite")
 
-# Get Secrets
+# Get Secretspytho
 MY_GITHUB_KEY = os.environ['MY_GITHUB_KEY']
 
 # Get Data
@@ -18,8 +18,8 @@ USERNAME = data['sender']['login']
 print('Invite user @'+USERNAME)
 
 # Send Request
-url = 'https://api.github.com/orgs/Hochschule-Mittweida/invitations/' + USERNAME
-payload=f"{'email': {USERNAME}}"
+url = 'https://api.github.com/orgs/Hochschule-Mittweida/memberships/' + USERNAME
+payload=''
 headers = {
     'Accept': 'application/vnd.github.v3+json',
     'Authorization': 'token '+MY_GITHUB_KEY
