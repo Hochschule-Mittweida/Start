f62091cfa52fd8498737bcdf5c930f24d2e880a9
Tom Schreiber <schreib3@hs-mittweida.de>
2021-08-12 11:03:31 +0200

    Ausgabe verbessert

--- invite/invite.py
+++ invite/invite.py
@@ -11,7 +11,7 @@ MY_GITHUB_KEY = os.environ['MY_GITHUB_KEY']
 # Get Data
 file = open(os.environ['GITHUB_EVENT_PATH'])
 data = json.load(file)
-print(f"GitHub-Data:{data}")
+#print(f"GitHub-Data:{data}")
 
 # Get Username
 USERNAME = data['sender']['login']
@@ -22,7 +22,8 @@ url = 'https://api.github.com/orgs/Hochschule-Mittweida/memberships/' + USERNAME
 payload=''
 headers = {
     'Accept': 'application/vnd.github.v3+json',
-    'Authorization': 'token '+MY_GITHUB_KEY
+    'Authorization': 'token ' + MY_GITHUB_KEY
 }
-response = requests.request("POST", url, headers=headers, data=payload)
+print(f"Request: {url}")
+response = requests.request("PUT", url, headers=headers, data=payload)
 print(response.text)
